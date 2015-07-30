4046f4564021c0f01a1bb0bda3e06e6dad5aee02
Jérémy Barbet <jeremgraph@gmail.com>
2015-07-30 12:09:31 +0200

    wip wip

--- app/views/content.php
+++ app/views/content.php
@@ -1,24 +1,13 @@
 <div class="blog-post" <?= the_wio_attributes() ?> style="background-color: <?= getColor();?>">
-
     <div class="container">
-
       <div class="left">
-
         <h3 class="blog-post-name"><?= project_name(); ?></h3>
-
-        <h2 class="blog-post-title">
-
-            <a href="<?php the_permalink(); ?>"><?php the_title_spans(); ?></a>
-
-        </h2>
-
+        <a class="blog-post-title" href="<?php the_permalink(); ?>"><?php the_title_spans(); ?></a>
         <?php single_post_shortlede(); ?>
       </div>
 
       <div class="right">
         <img src="<?= imgBandeau(); ?>" alt="">
       </div>
-
     </div>
-
 </div>
